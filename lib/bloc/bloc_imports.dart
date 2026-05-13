3a3989deb68616f413347730990cf88f1132207e
gandul-jago <ghaniyagst@gmail.com>
2026-05-13 12:54:52 +0700

    menambah fitur

--- lib/bloc/bloc_imports.dart
+++ lib/bloc/bloc_imports.dart
@@ -1,3 +1,4 @@
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:flutter_post_bloc_ti4ta/bloc/counter_bloc.dart';
+export 'package:flutter_post_bloc_ti4ta/bloc/my_observer.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_post_bloc_ti4ta/screen/home_screen.dart';
 import 'package:flutter_post_bloc_ti4ta/bloc/bloc_imports.dart';
+import 'package:flutter_post_bloc_ti4ta/screen/home_screen.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -22,7 +22,6 @@ class MyApp extends StatelessWidget {
         ), // ThemeData
         debugShowCheckedModeBanner: false,
         home: const HomeScreen(),
-        // home: const HomeScreenBlocConsumer(),
       ), // MaterialApp
     ); // BlocProvider
   }
